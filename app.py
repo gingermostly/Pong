3a75d773782e541a1b5b1716a0fbc961f210a708
gingermostly <gingermostly@gmail.com>
2019-12-06 18:54:01 -0500

    setup game screen and main loop

---
+++ app.py
@@ -0,0 +1,14 @@
+#Simple Pong app built in Python
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+#Main game loop
+
+while True:
+    wn.update() #update screen each time loop runs
