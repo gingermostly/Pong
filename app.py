382ad3ffc5f576d55b209bcac0c5d3a1e8933c82
gingermostly <gingermostly@gmail.com>
2020-04-20 19:38:15 -0400

    create ball movement

--- app.py
+++ app.py
@@ -33,6 +33,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.1
+ball.dy = 0.1
 
 #Function for moving paddles
 
@@ -45,7 +47,6 @@ def paddle_a_down():
     y = paddle_a.ycor()
     y -= 20
     paddle_a.sety(y)
-
 
 def paddle_b_up():
     y = paddle_b.ycor()
@@ -68,3 +69,6 @@ wn.onkeypress(paddle_b_down, "Down") #call paddle_b_down function when down arro
 
 while True:
     wn.update() #update screen each time loop runs
+    #Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
