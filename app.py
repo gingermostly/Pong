8ccbbba5f6b39239718ecdd38784d88e4f146322
gingermostly <gingermostly@gmail.com>
2020-04-20 19:09:42 -0400

    create first paddle

--- app.py
+++ app.py
@@ -6,7 +6,20 @@ wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
-wn.tracer(0)
+wn.tracer(0) #stop window from updating to improve speed and performance
+
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0) #set animation speed to maximum
+paddle_a.shape("square") #give paddle shape using built-in square shape
+paddle_a.color("white")
+paddle_a.penup() #prevent line from being drawn
+paddle_a.goto(-350, 0) #vertically center paddle in middle of screen
+paddle_a.shapesize(stretch_wid=5, stretch_len=1) #stretch paddle width to 100px (5 times 20px default)
+
+#Paddle B
+
+#Ball
 
 #Main game loop
 
