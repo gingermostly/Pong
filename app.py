53743e5b42d66573b9ee9ce4ddf8006dd12f6005
gingermostly <gingermostly@gmail.com>
2020-04-20 19:17:04 -0400

    finish paddles and ball

--- app.py
+++ app.py
@@ -18,8 +18,21 @@ paddle_a.goto(-350, 0) #vertically center paddle in middle of screen
 paddle_a.shapesize(stretch_wid=5, stretch_len=1) #stretch paddle width to 100px (5 times 20px default)
 
 #Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0) 
+paddle_b.shape("square") 
+paddle_b.color("white")
+paddle_b.penup()
+paddle_b.goto(350, 0) 
+paddle_b.shapesize(stretch_wid=5, stretch_len=1) 
 
 #Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
 
 #Main game loop
 
