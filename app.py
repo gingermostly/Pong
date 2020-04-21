e032236e3dedf12c9e051d2c36e80ca16e6cea11
gingermostly <gingermostly@gmail.com>
2020-04-20 20:19:43 -0400

    create coord checks for ball and paddle collisions

--- app.py
+++ app.py
@@ -90,3 +90,11 @@ while True:
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+
+    #Paddle and ball collisions
+    if (ball.xcor() > 340 and ball.xcor() > 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.xcor() > paddle_b.xcor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.xcor() > paddle_a.xcor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
